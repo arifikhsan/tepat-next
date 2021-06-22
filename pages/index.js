bee207329daf3bb3f841ea5308e402050b0c8053
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-06-22 10:43:19 +0700

    ⚡

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Tepat. Teknisi Cepat Tanggap.</title>
+        <title>⚡ Tepat. Teknisi Listrik Cepat Tanggap.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
         <h1 className="text-6xl font-bold">
-          <span>Tepat. </span>
-          <span className="text-blue-600">Teknisi Cepat Tanggap. </span>
+          <span>⚡ Tepat. </span>
+          <span className="text-blue-600">Teknisi Listrik Cepat Tanggap. </span>
         </h1>
         <h1 className="mt-8 text-6xl font-bold">
           <span>Segera datang!</span>
